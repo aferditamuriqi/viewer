7e38748ca615078a24b3b2e78dbc55ac1ecbc337
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2018-12-19 09:34:39 +0100

    updated

---
+++ docs/material.js
@@ -0,0 +1,27 @@
+
+M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function() {
+  let elements = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(elements, {accordion: true});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  let elements = document.querySelectorAll('.dropdown-trigger');
+  M.Dropdown.init(elements, {
+    alignment: 'right',
+    constrainWidth: false,
+    coverTrigger: false,
+    closeOnClick: false
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  let elements = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(elements, {'edge': 'left'});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  let elements = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(elements, {'edge': 'left'});
+});
